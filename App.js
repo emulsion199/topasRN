9c74fb8f4c18ed996c4796f3a1129ff0a29b83f5
emulsion199 <wodnjs471@naver.com>
2022-03-22 15:09:09 +0900

    Set Navigation

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'; 
+import { createStackNavigator } from '@react-navigation/stack'; 
+import HomeScreen from './Components/HomeScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createStackNavigator();
+const App=()=>
+{
+  return(
+    <NavigationContainer> 
+        	{/* 네비게이션 기본틀의 스택을 생성 */} 
+            <Stack.Navigator> 
+            	{/* 해당스택에 들어갈 화면 요소를 넣어준다. */}
+                <Stack.Screen name="Home" component={HomeScreen}/> 
+            </Stack.Navigator> 
+        </NavigationContainer> 
+  )
 }
 
 const styles = StyleSheet.create({
@@ -19,3 +27,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+export default App
